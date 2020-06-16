96e2d378593526f59adc23a6edea3edcbc3339d5
awjae <djwotmd@gmail.com>
2020-06-16 15:42:34 +0900

    mainImage

--- movieApp/client/src/components/Config.js
+++ movieApp/client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-
+export const API_KEY = 'f5cd9d5e871c6ecf03657693c30c911c';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
--- movieApp/client/src/components/views/LandingPage/LandingPage.js
+++ movieApp/client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
-import { API_URL } from '../../Config';
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
 
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
     useEffect (() => {
-        const endPoint = `${API_URL}`
-    }, [input])
+        const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        fetch(endPoint)
+        .then(response => response.json())
+        .then(response => {
+            
+            console.log(response)
+            
+            setMovies([...response.results])
+            setMainMovieImage(response.results[0])
+
+        })  
+
+    }, [])
     
     return (
         <>
         <div style={{ windth: '100%' , margin: '0'}}>
+        {MainMovieImage && 
+            <MainImage 
+                image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                title={MainMovieImage.original_title}
+                text={MainMovieImage.overview}>
+            </MainImage>
+        }
+
             <div style={{ width: '85%', margin: '1rem auto'}}>
                 <h2>Movie by lastest</h2>
                 <hr />
@@ -19,7 +42,7 @@ function LandingPage() {
             </div>
         
             <div style={{ display: 'flex', justifyContent: 'center'}}>
-            <button>Load More</button>
+                <button>Load More</button>
             </div>
 
         </div>
