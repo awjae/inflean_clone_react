dc8ed0f2aee0c409cfb96996ae8b639c2edd7d5f
awjae <djwotmd@gmail.com>
2020-06-17 15:46:37 +0900

    useGrid

--- movieApp/client/src/components/views/LandingPage/LandingPage.js
+++ movieApp/client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCard from '../../common/GridCard';
+import { Row } from 'antd';
 
 function LandingPage() {
 
@@ -38,6 +40,23 @@ function LandingPage() {
                 <h2>Movie by lastest</h2>
                 <hr />
 
+                <Row gutter={[16, 16]}>
+
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCard
+                                image={movie.poster_path ? 
+                                `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}>
+                                    
+                            </GridCard>
+
+                        </React.Fragment>
+
+                    ))}
+
+                </Row>
 
             </div>
         
