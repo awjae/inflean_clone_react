2c1aa18b5af5dd269e631ee98538834a5cbc589b
awjae <djwotmd@gmail.com>
2020-06-17 18:03:37 +0900

    actor

--- movieApp/client/src/components/common/GridCard.js
+++ movieApp/client/src/components/common/GridCard.js
@@ -2,15 +2,28 @@ import React from 'react'
 import { Col } from 'antd'
 
 function GridCard(props) {
-    return (
-        <Col lg={6} md={8} xs={24}>
-            <div style={{ position: 'relative' }}>
-                <a href={`/movie/${props.movieId}`}>
-                    <img style={{width: '100%', height:'320px'}} src={props.image} alt={props.moviName}></img>
-                </a>
-            </div>
-        </Col>
-    )
+
+    if (props.landingPage) {
+
+        return (
+            <Col lg={6} md={8} xs={24}>
+                <div style={{ position: 'relative' }}>
+                    <a href={`/movie/${props.movieId}`}>
+                        <img style={{width: '100%', height:'320px'}} src={props.image} alt={props.movieName}></img>
+                    </a>
+                </div>
+            </Col>
+        )
+    } else {
+        return (
+            <Col lg={6} md={8} xs={24}>
+                <div style={{ position: 'relative' }}>
+                    <img style={{width: '100%', height:'320px'}} src={props.image} alt={props.characterName}></img>
+                </div>
+            </Col>
+        )
+    }
+
 }
 
 export default GridCard
---
+++ movieApp/client/src/components/views/MovieDatail/MovieDetail.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react'
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from '../LandingPage/Sections/MainImage';
+import MovieInfo from './Sections/MovieInfo';
+import GridCard from '../../common/GridCard';
+import { Row } from 'antd';
+
+function MovieDetail(props) {
+
+    let movieId = props.match.params.movieId
+    const [Movie, setMovie] = useState([])
+    const [Casts, setCasts] = useState([])
+    const [ActorToggle, setActorToggle] = useState(false)
+
+    const toggleActorView = () => {
+        setActorToggle(!ActorToggle)
+    }
+
+    useEffect(() => {
+
+        let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+
+        let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+
+        fetch(endpointInfo)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                setMovie(response)
+                
+            })
+
+        fetch(endpointCrew)
+            .then(response => response.json())
+            .then(response => {
+                setCasts(response.cast)
+                
+            })
+
+    }, [])
+
+
+    return (
+        <div>
+            <MainImage
+                image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
+                title={Movie.original_title}
+                text={Movie.overview}>    
+            </MainImage>
+            <div style={{ width:'85%', margin: '1rem auto' }}>
+                <MovieInfo
+                    movie={Movie}>
+                </MovieInfo>
+                <br></br>
+                
+                <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
+                    <button onClick={toggleActorView}> Toggle Actor View </button>
+                </div>
+
+                {ActorToggle && 
+                    <Row gutter={[16, 16]}>
+                        
+                        {Casts && Casts.map((cast, index) => (
+                            <React.Fragment key={index}>
+                                <GridCard
+                                    image={cast.profile_path ? 
+                                    `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                                    characterName={cast.name}>
+                                        
+                                </GridCard>
+
+                            </React.Fragment>
+
+                        ))}
+
+                    </Row>
+                }
+                
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default MovieDetail
